ff2798526f2b132589a56afc2c91ebc022f1398c
Casey Hofland <hofland.casey@gmail.com>
2020-10-14 16:49:16 +0200

    Kitchen Scene made VR ready

---
+++ NewTech_CaseyRandyNoxNouri/Assets/SceneReloader.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneReloader : MonoBehaviour
+{
+    public float waitRealtime = 5f;
+
+    public void ReloadCurrentScene()
+    {
+        StartCoroutine(Coroutine());
+
+        IEnumerator Coroutine()
+        {
+            yield return new WaitForSecondsRealtime(waitRealtime);
+
+            var currentScene = SceneManager.GetActiveScene();
+            SceneManager.LoadScene(currentScene.buildIndex);
+        }
+    }
+}
